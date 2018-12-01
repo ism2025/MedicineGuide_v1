e7d355c683933a4b02bf20ef26e7a904d5e56f19
yosefmoq <yos.ail@outlook.com>
2018-12-01 11:47:04 +0200

    change relative layout to view pager in Main activity and use View page adapter

--- app/src/main/java/com/example/ismailamassi/finalprojectandroid/Activites/MainActivity.java
+++ app/src/main/java/com/example/ismailamassi/finalprojectandroid/Activites/MainActivity.java
@@ -6,10 +6,14 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.TextView;
 
+import com.example.ismailamassi.finalprojectandroid.Adapters.ViewPagerAdapter;
 import com.example.ismailamassi.finalprojectandroid.Fragments.HomePageFragment;
 import com.example.ismailamassi.finalprojectandroid.Fragments.NotificationsPageFragment;
 import com.example.ismailamassi.finalprojectandroid.Fragments.PatientPageFragment;
@@ -20,6 +24,14 @@ import com.example.ismailamassi.finalprojectandroid.R;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    MenuItem prevMenuItem;
+    ViewPager viewPager;
+    MenuItem PrevMenuItem;
+    HomePageFragment homePageFragment;
+    PatientPageFragment patientPageFragment;
+    NotificationsPageFragment notificationsPageFragment;
+    PharmaceuticalPageFragment pharmaceuticalPageFragment;
+    SavedPageFragment savedPageFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -28,19 +40,19 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    setFragment(new HomePageFragment());
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.navigation_Patient:
-                    setFragment(new PatientPageFragment());
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.navigation_notifications:
-                    setFragment(new NotificationsPageFragment());
+                    viewPager.setCurrentItem(2);
                     return true;
                 case R.id.navigation_pharmaceutical:
-                    setFragment(new PharmaceuticalPageFragment());
+                    viewPager.setCurrentItem(3);
                     return true;
                 case R.id.navigation_saves:
-                    setFragment(new SavedPageFragment());
+                    viewPager.setCurrentItem(4);
                     return true;
             }
             return false;
@@ -52,16 +64,53 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        viewPager = findViewById(R.id.view_pagerbottomnav);
+
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setItemBackgroundResource(R.color.colorPrimaryDark);
-        setFragment(new HomePageFragment());
+        viewPager.setCurrentItem(0);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(prevMenuItem!=null){
+                    prevMenuItem.setChecked(false);
+
+                } else {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                navigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = navigation.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setupViewPager(viewPager);
+
     }
 
-    public void setFragment(Fragment fragment){
-        getSupportFragmentManager().beginTransaction().replace(R.id.view_pagerbottomnav,fragment).commit();
-
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        homePageFragment = new HomePageFragment();
+        patientPageFragment = new PatientPageFragment();
+        notificationsPageFragment=new NotificationsPageFragment();
+        pharmaceuticalPageFragment = new PharmaceuticalPageFragment();
+        savedPageFragment = new SavedPageFragment();
+        viewPagerAdapter.addFragment(homePageFragment);
+        viewPagerAdapter.addFragment(patientPageFragment);
+        viewPagerAdapter.addFragment(notificationsPageFragment);
+        viewPagerAdapter.addFragment(pharmaceuticalPageFragment);
+        viewPagerAdapter.addFragment(savedPageFragment);
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
